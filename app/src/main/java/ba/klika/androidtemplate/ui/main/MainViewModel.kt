dd5353f02e2aa5bd73a65e76c897ef90096fcc5b
Ensar Sarajčić <ensar.sarajcic@klika.ba>
2018-11-08 22:22:44 +0100

    Add log out action

---
+++ app/src/main/java/ba/klika/androidtemplate/ui/main/MainViewModel.kt
@@ -0,0 +1,26 @@
+package ba.klika.androidtemplate.ui.main
+
+import ba.klika.androidtemplate.data.session.SessionRepository
+import ba.klika.androidtemplate.scheduling.SchedulingProvider
+import ba.klika.androidtemplate.ui.base.viewmodel.BaseViewModel
+import ba.klika.androidtemplate.ui.base.viewmodel.SingleLiveEvent
+import javax.inject.Inject
+
+/**
+ * @author Ensar Sarajčić <ensar.sarajcic@klika.ba>.
+ */
+class MainViewModel
+@Inject constructor(
+        private val sessionRepository: SessionRepository,
+        schedulingProvider: SchedulingProvider): BaseViewModel(schedulingProvider) {
+
+    val landingNavigationTrigger = SingleLiveEvent<Void>()
+
+    fun onLogOutClick() {
+        sessionRepository.logOut()
+                .asIOCall()
+                .subscribe {
+                    landingNavigationTrigger.call()
+                }.disposeOnClear()
+    }
+}
