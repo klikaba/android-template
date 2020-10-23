61d3749ecb0f0303eec4740c8073d1e40ee32c33
Ensar Sarajčić <es.ensar@gmail.com>
2020-10-23 22:51:38 +0200

    Move build logic to kotlin

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,7 @@
+object AndroidConfig {
+    object Versions {
+        const val androidCompileSdkVersion = 30
+        const val androidMinSdkVersion = 18
+        const val androidTargetSdkVersion = 30
+    }
+}
